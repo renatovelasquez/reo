6dbd78991c745c87a6af0626f4378dcd2598c7d5
Renato Velasquez <renato@xiberty.com>
2015-12-12 15:46:01 -0400

    php contact

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'revelsix@gmail.com';
+$siteTitle = 'CONTACTO';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
